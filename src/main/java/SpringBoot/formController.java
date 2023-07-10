b45aa6fbd74428faf83a7eb4bd606062aa9967a6
naoki <naoki@localhost>
2023-07-10 20:29:53 +0900

    テスト

--- src/main/java/SpringBoot/formController.java
+++ src/main/java/SpringBoot/formController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class formController {
 
-
 	@GetMapping("/login")
     public String showLoginPage() {
         return "login.html";
